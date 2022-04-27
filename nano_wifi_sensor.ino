a7d46e8cb68051fe30056637f4382d936f63e3de
Jeff Prevost <jeff.prevost@utsa.edu>
2022-04-27 11:34:07 -0500

    syncing new files

---
+++ nano_wifi_sensor.ino
@@ -0,0 +1,236 @@
+/*
+
+Connect to local wifi network
+
+Send DHT11 sensor information across network
+
+Jeff Prevost
+
+1/15/2021
+
+Validated 3/29/2022
+
+ */
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include "arduino_secrets.h"
+
+#define DHTTYPE    DHT11      // DHT 11 Sensor type
+#define DHTPIN 7              // Digital pin connected to the DHT sensor 
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+void setup() {
+
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  // Initialize device.
+  dht.begin();
+  
+  while (!Serial) {
+
+    ; // wait for serial port to connect. Needed for native USB port only
+
+  }
+
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+
+  // check for the WiFi module:
+
+  if (WiFi.status() == WL_NO_MODULE) {
+
+    Serial.println("Communication with WiFi module failed!");
+
+    // don't continue
+
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+
+    Serial.println("Please upgrade the firmware");
+
+  }
+
+  // attempt to connect to Wifi network:
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to WPA SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network:
+
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
+    delay(10000);
+
+  }
+
+  // you're connected now, so print out the data:
+
+  Serial.print("You're connected to the network");
+
+  printCurrentNet();
+
+  printWifiData();
+
+}
+
+void loop() {
+
+  // check the network connection once every 10 seconds:
+
+  delay(1000);
+
+  printCurrentNet();
+  printSensorData();
+}
+
+void printSensorData() {
+
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature*9/5 + 32);
+    Serial.println(F("°F"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+}
+
+void printWifiData() {
+
+  // print your board's IP address:
+
+  IPAddress ip = WiFi.localIP();
+
+  Serial.print("IP Address: ");
+
+  Serial.println(ip);
+
+  Serial.println(ip);
+
+  // print your MAC address:
+
+  byte mac[6];
+
+  WiFi.macAddress(mac);
+
+  Serial.print("MAC address: ");
+
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+
+  // print the SSID of the network you're attached to:
+
+  Serial.print("SSID: ");
+
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+
+  byte bssid[6];
+
+  WiFi.BSSID(bssid);
+
+  Serial.print("BSSID: ");
+
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+
+  long rssi = WiFi.RSSI();
+
+  Serial.print("signal strength (RSSI):");
+
+  Serial.println(rssi);
+
+  // print the encryption type:
+
+  byte encryption = WiFi.encryptionType();
+
+  Serial.print("Encryption Type:");
+
+  Serial.println(encryption, HEX);
+
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+
+  for (int i = 5; i >= 0; i--) {
+
+    if (mac[i] < 16) {
+
+      Serial.print("0");
+
+    }
+
+    Serial.print(mac[i], HEX);
+
+    if (i > 0) {
+
+      Serial.print(":");
+
+    }
+
+  }
+
+  Serial.println();
+}
